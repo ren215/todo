ee73163f303dc1b7e7838c52f59636e3d46b7cc2
ren <asupc38@gmail.com>
2025-01-26 12:47:22 +0900

    ヘッダー作成

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+
+import { cva } from '../../../styled-system/css';
+
+// style
+const HeaderStyle = cva({
+  base: {
+    bg: 'pink',
+    w: '100%',
+    h: '50px',
+    pl: '5px',
+    pr: '5px',
+    zIndex: '5',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
+
+// type
+type Props = {
+  items: {
+    left?: ReactNode;
+    center?: ReactNode;
+    right?: ReactNode;
+  };
+};
+
+const Header = (props: Props) => {
+  const { items } = props;
+
+  return (
+    <div className={HeaderStyle()}>
+      <div>{items.left}</div>
+      <div>{items.center}</div>
+      <div>{items.right}</div>
+    </div>
+  );
+};
+
+export default Header;
