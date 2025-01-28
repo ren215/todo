a3ef3e9f4f88c26da18da56befc93616c93b4f5d
ren <asupc38@gmail.com>
2025-01-28 23:33:50 +0900

    todoTypeの修正(仮)

--- frontend/src/types/todoType.ts
+++ frontend/src/types/todoType.ts
@@ -1,15 +1,18 @@
 export type todoType = {
-  state: stateType; // 状態
+  id: string;
+  state: StateType; // 状態
   title: string; // タイトル
   creator: string; // 作成者
   applicable: string[]; // 該当者
   endDate: Date; // 期日
 };
 
-type stateType = {
-  notStarted: string; // 未着手
-  inProgress: string; // 進行中
-  pendingApproval: string; // 承認待ち
-  completed: string; // 完了
-  onHold: string; // 保留
-};
+// type stateType = {
+//   notStarted: string; // 未着手
+//   inProgress: string; // 進行中
+//   pendingApproval: string; // 承認待ち
+//   completed: string; // 完了
+//   onHold: string; // 保留
+// };
+
+type StateType = 'notStarted' | 'inProgress' | 'pendingApproval' | 'completed' | 'onHold';
