dffe733ca6b3ae1225cc8e097af49b797648c802
ren <asupc38@gmail.com>
2025-01-28 23:31:59 +0900

    ボタンコンポーネントのpropsにbgを追加

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -5,12 +5,15 @@ import { sizeType } from '../types/sizeType';
 
 // style
 const buttonStyle = cva({
-  base: { fontSize: '14px', bg: 'pink', textAlign: 'center', borderRadius: '8px', _hover: { opacity: 0.9 } },
+  base: { fontSize: '14px', bg: 'blue', textAlign: 'center', borderRadius: '8px', _hover: { opacity: 0.9 } },
   variants: {
     size: {
       small: { w: '63px', h: '24px' },
       medium: { w: '79px', h: '32px' },
       large: { w: '86px', h: '40px', fontSize: '16px' },
+    },
+    bg: {
+      white: { bg: 'white', border: '1px solid black' },
     },
     disabled: {
       true: { bg: 'silver', cursor: 'not-allowed', color: 'rgba(0, 0, 0, 0.4)' },
@@ -21,15 +24,16 @@ const buttonStyle = cva({
 type Props = {
   children: ReactNode;
   size?: sizeType;
+  bg?: 'white';
   disabled?: boolean;
   onClick?: () => void;
 };
 
 const Button = (props: Props) => {
-  const { children, size = 'medium', disabled, onClick } = props;
+  const { children, size = 'medium', disabled, bg, onClick } = props;
 
   return (
-    <button className={buttonStyle({ disabled: disabled, size: size })} disabled={disabled} onClick={onClick}>
+    <button className={buttonStyle({ disabled: disabled, size: size, bg: bg })} disabled={disabled} onClick={onClick}>
       {children}
     </button>
   );
