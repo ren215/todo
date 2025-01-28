7d4e2341418c80a209ee3a6bd67867254d3bcc27
ren <asupc38@gmail.com>
2025-01-28 23:33:32 +0900

    formatDate()の実装

---
+++ frontend/src/utils/formatDate.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: Date) => {
+  const newDate = new Date(date);
+  const year = newDate.getFullYear();
+  const month = String(newDate.getMonth() + 1).padStart(2, '0');
+  const day = String(newDate.getDate()).padStart(2, '0');
+
+  return `${year}/${month}/${day}`;
+};
